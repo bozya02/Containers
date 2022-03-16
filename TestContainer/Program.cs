2a5ff38a8899c54ca8e07d0171334d30b698ca55
201913 <bozinandrey1@yandex.ru>
2022-03-16 11:01:35 +0300

    first commit

---
+++ TestContainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
